ba70a6d3da310c4f807d1185ed537ac520d21659
Pedro Torchio <pedrotorchio@gmail.com>
2026-01-04 22:14:37 +1100

    fixing spy

--- src/general/devtools.ts
+++ src/general/devtools.ts
@@ -1,12 +1,9 @@
-import { consola } from "consola";
-
 export const spy = (fn: (...args: any[]) => void, message?: string) => {
-  const logger = consola.withTag(message || fn.name || "spy");
   return (...args: any[]) => {
     console.group(message || fn.name || "spy");
-    logger.log("Running with args:", ...args);
+    console.log("Running with args:", ...args);
     const result = fn(...args);
-    logger.log("Result:", result);
+    console.log("Result:", result);
     console.groupEnd();
     return result;
   };
